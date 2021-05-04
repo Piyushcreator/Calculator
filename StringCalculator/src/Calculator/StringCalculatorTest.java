d9efd03d96cf4bb60bc829b91b1187e44c6c69df
Piyushcreator <piyushshrm68@gmail.com>
2021-05-04 21:05:53 +0530

    Adding Multiple Numbers

--- StringCalculator/src/Calculator/StringCalculatorTest.java
+++ StringCalculator/src/Calculator/StringCalculatorTest.java
@@ -12,4 +12,22 @@ public class StringCalculatorTest {
 		assertEquals(0,StringCalculator.Add(""));
 	}		
 	
+	@Test
+	public void checkForSingleNumber()
+	{
+		assertEquals(1,StringCalculator.Add("1"));
+	}
+	
+	@Test
+	public void checkForTwoNumber()
+	{
+		assertEquals(3,StringCalculator.Add("1,2"));
+	}
+	
+	@Test
+	public void checkForMultiplenumber()
+	{
+		assertEquals(6,StringCalculator.Add("1,2,3"));
+	}
+	
 }
